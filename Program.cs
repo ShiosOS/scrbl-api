9d46e9cf042eaa586358ccaff35f713ff25e6dd7
ShiosOS <juliuswalton7901@gmail.com>
2025-07-17 01:30:51 +0000

    edit for ssl connection

--- Program.cs
+++ Program.cs
@@ -1,27 +1,39 @@
+using Microsoft.AspNetCore.HttpOverrides;
+using System.Net;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto,
+    KnownNetworks = { new Microsoft.AspNetCore.HttpOverrides.IPNetwork(IPAddress.Parse("172.19.0.0"), 16) }
+});
+
+app.UseAuthorization()
+
+app.MapControllers();
+
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger(); 
+    app.UseSwaggerUI(); 
 }
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
+app.MapGet("/WeatherForecast", () =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
@@ -31,8 +43,9 @@ app.MapGet("/weatherforecast", () =>
         .ToArray();
     return forecast;
 })
-.WithName("GetWeatherForecast");
-
+.WithName("GetWeatherForecast")
+.WithOpenApi(); 
+               
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
